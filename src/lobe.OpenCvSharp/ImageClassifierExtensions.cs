35639c3f51237173a0040db7c25d4329c9c5e452
Diego Colombo <dicolomb@microsoft.com>
2021-01-16 16:10:37 +0000

    add opencv support

---
+++ src/lobe.OpenCvSharp/ImageClassifierExtensions.cs
@@ -0,0 +1,13 @@
+﻿using OpenCvSharp;
+
+namespace lobe.OpenCvSharp
+{
+    public static class ImageClassifierExtensions
+    {
+        public static ClassificationResults Classify(this ImageClassifier classifier,
+            Mat image)
+        {
+            return classifier.Classify(image.ToFlatArrayMatchingInputShape(classifier.Signature, "Image"));
+        }
+    }
+}
