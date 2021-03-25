82cfa32ea3df7e04a99b1218692ce36fb1c14dea
Diego Colombo <dicolomb@microsoft.com>
2021-03-25 02:38:04 +0000

    updated parser to 0.9

--- src/lobe.Http.Tests/ResultParsingTests.cs
+++ src/lobe.Http.Tests/ResultParsingTests.cs
@@ -10,90 +10,37 @@ namespace lobe.Http.Tests
         public void can_deserialize()
         {
             var src = @"{
-    ""outputs"": {
-        ""Labels"": [
-            [
-                ""Dragonoid Pyrus"",
-                2.9784330557447447e-10
-            ],
-            [
-                ""Fangzor Aquos"",
-                5.896686134168605e-13
-            ],
-            [
-                ""Gorthion Haos"",
-                4.2895406671128455e-10
-            ],
-            [
-                ""Gorthion Ventus"",
-                1.5067771160522222e-16
-            ],
-            [
-                ""Howlkor Ultra Haos"",
-                4.38083805343048e-11
-            ],
-            [
-                ""Hydorous Ultra Pyrus "",
-                1.7968190378235462e-15
-            ],
-            [
-                ""Hydorous X Thryno Ultra Pyrus Aurelus"",
-                2.984291711527476e-07
-            ],
-            [
-                ""Lupitheon Ultra Aurelus"",
-                0.8843237161636353
-            ],
-            [
-                ""Nillius Haos"",
-                1.99084368782132e-12
-            ],
-            [
-                ""No Bakugan"",
-                2.932698862423422e-06
-            ],
-            [
-                ""Pegatrix Aurelus"",
-                5.779965807188592e-13
-            ],
-            [
-                ""Pegratrix X Goreen Ultra Haos / Aurelus"",
-                8.831047892954302e-09
-            ],
-            [
-                ""Phaedrus Pyrus"",
-                0.031188247725367546
-            ],
-            [
-                ""Pyravian Ultra Aurelus"",
-                0.08448425680398941
-            ],
-            [
-                ""Serpenteze Aquos"",
-                4.100685763175349e-11
-            ],
-            [
-                ""Thryno Pyrus"",
-                5.200028567742265e-07
-            ],
-            [
-                ""Vicerox Ultra Haos"",
-                7.508459409777402e-10
-            ]
-        ],
-        ""Prediction"": [
-            ""Lupitheon Ultra Aurelus""
-        ]
-    }
+    ""predictions"": [
+        {
+            ""label"": ""Aurelus Lupitheon Ultra (Battle Planet)"",
+            ""confidence"": 1
+        },
+        {
+            ""label"": ""Aquos Hydorous (Battle Planet)"",
+            ""confidence"": 0
+        },
+        {
+            ""label"": ""Aquos Trox Ultra (Battle Planet)"",
+            ""confidence"": 0
+        },
+        {
+            ""label"": ""Aurelus Pyravian Ultra (Battle Planet)"",
+            ""confidence"": 0
+        },
+        {
+            ""label"": ""Darkus Hydorous (Battle Planet)"",
+            ""confidence"": 0
+        }
+    ]
 }";
 
             var result = src.ToClassificationResults();
 
             result.Should().NotBeNull();
-            result.Prediction.Label.Should().Be("Lupitheon Ultra Aurelus");
+            result.Prediction.Label.Should().Be("Aurelus Lupitheon Ultra (Battle Planet)");
             result.Classifications.Select(c => c.Label).Should().BeEquivalentTo(
-            
-                "Dragonoid Pyrus","Fangzor Aquos","Gorthion Haos","Gorthion Ventus","Howlkor Ultra Haos","Hydorous Ultra Pyrus ","Hydorous X Thryno Ultra Pyrus Aurelus","Lupitheon Ultra Aurelus","Nillius Haos","No Bakugan","Pegatrix Aurelus","Pegratrix X Goreen Ultra Haos / Aurelus","Phaedrus Pyrus","Pyravian Ultra Aurelus","Serpenteze Aquos","Thryno Pyrus","Vicerox Ultra Haos"
+
+                "Aurelus Lupitheon Ultra (Battle Planet)", "Aquos Hydorous (Battle Planet)", "Aquos Trox Ultra (Battle Planet)", "Aurelus Pyravian Ultra (Battle Planet)", "Darkus Hydorous (Battle Planet)"
             );
         }
     }
